b07a35297efa584f5f1f838f5d3c8ec807aa0a9e
Ratiya Nagarjun Bhurabhai <23it109@charusat.edu.in>
2025-07-27 14:19:14 +0530

    s

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,55 +1,61 @@
-import User from "@/models/User";
-import db from "@/utils/db";
-import bcryptjs from "bcryptjs";
+import User from '../../../models/User';
+import db from '../../../utils/db';
+import bcryptjs from 'bcryptjs';
 
-const handler = async (req, res) => {
-  if (req.method !== "POST") {
+async function handler(req, res) {
+  // We only want to allow POST requests to this endpoint
+  if (req.method !== 'POST') {
     return;
   }
 
   const { name, email, password } = req.body;
 
+  // Basic validation
   if (
     !name ||
     !email ||
-    !email.includes("@") ||
+    !email.includes('@') ||
     !password ||
     password.trim().length < 5
   ) {
     res.status(422).json({
-      message: "Validation error",
+      message: 'Validation error',
     });
     return;
   }
-  await db.connect();
-  const existingUser = await User.findOne({ email: email });
 
+  // Connect to the database
+  await db.connect();
+
+  // Check if the user already exists
+  const existingUser = await User.findOne({ email: email });
   if (existingUser) {
-    res.status(422).json({
-      message: "User exists already",
-    });
+    res.status(422).json({ message: 'User exists already!' });
     await db.disconnect();
     return;
   }
 
-  const newUser = await User({
+  // Create a new user
+  const newUser = new User({
     name,
     email,
+    // Hash the password before saving
     password: bcryptjs.hashSync(password),
     isAdmin: false,
   });
 
+  // Save the new user to the database
   const user = await newUser.save();
-
   await db.disconnect();
 
+  // Send a success response
   res.status(201).send({
-    message: "Created user",
+    message: 'Created user!',
     _id: user._id,
     name: user.name,
     email: user.email,
     isAdmin: user.isAdmin,
   });
-};
+}
 
 export default handler;
